4cce8680b0e83f02906bcfcdcd19541ce5b4cf58
fatemeafashari <fati_afshari@yahoo.com>
2022-07-06 16:54:21 +0430

    first commit

---
+++ app/src/main/java/com/example/persiandatepicker/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.persiandatepicker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.datepicker.ListItemPicker
+import com.example.persiandatepicker.ui.theme.PersianDatePickerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PersianDatePickerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background) {
+                    val possibleValues = listOf("فروردین", "اردیبهشت" , "خرداد" , "تیر", "مرداد")
+                    var state by remember { mutableStateOf(possibleValues[0]) }
+                    ListItemPicker(
+                        label = { it },
+                        value = state,
+                        onValueChange = { state = it },
+                        list = possibleValues
+                    )
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    PersianDatePickerTheme {
+        Greeting("Android")
+    }
+}
